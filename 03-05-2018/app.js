d0604f2dd8df5ff93158a93f81899434a90794ed
elmerpino <jpino@ucol.mx>
2018-05-03 08:40:48 -0500

    Add files via upload

---
+++ 03-05-2018/app.js
@@ -0,0 +1,12 @@
+const request = require('request');
+
+request({
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=174%20pablo%20neruda%20colima%2028050',
+    json: true
+}, (error, response, body) =>{
+    // Es el primero que hicimos console.log(body);
+    // Es el segundo que hicimos console.log(JSON.stringify(body, undefined, 2));
+    // Es el tercero que hicimos console.log(JSON.stringify(response, undefined, 2));
+    // Es el cuarto que hicimos console.log(JSON.stringify(error, undefined, 2));
+    console.log('Address: ${body.results[0].formatted_address}');
+});
